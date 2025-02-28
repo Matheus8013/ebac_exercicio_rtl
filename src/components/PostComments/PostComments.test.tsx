fe3169aff48e3d59490605a224ed262f592c24fa
Matheus Ferreira <matheusfeereirabispo1380@gmail.com>
2025-02-28 19:31:24 -0300

    adicionando o teste para a realização de comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -3,8 +3,28 @@ import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
+    test('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    test('Deve adicionar dois comentarios ao post', () => {
+        render(<PostComment />)
+
+        fireEvent.change(screen.getByTestId('area-comentario'), {
+            target: {
+                value: 'Primeiro comentário adicionado'
+            }
+        });
+        fireEvent.click(screen.getByTestId('btn-comentario'));
+
+        fireEvent.change(screen.getByTestId('area-comentario'), {
+            target: {
+                value: 'Segundo comentário adicionado'
+            }
+        });
+        fireEvent.click(screen.getByTestId('btn-comentario'))
+
+        expect(screen.getAllByTestId('comentario')).toHaveLength(2)
+    })
 });
